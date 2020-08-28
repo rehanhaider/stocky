13ed76752cf31d5633ba0d7ac4bba232040c27cf
justgoodin <=>
2020-08-29 02:12:09 +0530

    NA

---
+++ course/notes.py
@@ -0,0 +1,18 @@
+import numpy 
+import pandas
+import matplotlib #To visualise pandas dataframes
+
+import math
+import random
+import statsmodel #Statistical models
+
+import pandas_datareader #Read finance data
+
+#Online Sources
+#       Paid                        Free
+#   well-structured data        good enough data quality
+#   no missing values           not precise
+
+#Sources of Financial data
+#IEX, morningstar:Free, alphavantage:Free, quandl, yahooFinance
+#Using pandas-datareader
