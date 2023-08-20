fc391377f3ed8b8cff2ed93cb66343b1a05dd775
Rehan Haider <email@rehanhaider.com>
2023-08-21 00:36:47 +0530

    FEAT: Script to create project folder structure for input an doutput

--- lib/structure.py
+++ lib/structure.py
@@ -4,6 +4,7 @@ Creates folder structure for the project
 
 
 from pathlib import Path
+from .config import get_config
 
 
 def create_folders():
@@ -11,8 +12,17 @@ def create_folders():
     Creates the folder structure for the project
     """
     # Create a directory named data in the current directory
-    Path("data/maket_data/zerodha").mkdir(parents=True, exist_ok=True)
-    Path("data/maket_data/yahoo").mkdir(parents=True, exist_ok=True)
-    Path("data/maket_data/bse").mkdir(parents=True, exist_ok=True)
-    Path("data/maket_data/nse").mkdir(parents=True, exist_ok=True)
-    Path("output").mkdir(parents=True, exist_ok=True)
+
+    input_paths: dict = get_config()["paths"]["input"]
+    output_path: str = get_config()["paths"]["output"]
+
+    # for key, value in input_paths.items():
+    #    Path(value).mkdir(parents=True, exist_ok=True)
+
+    # for key, value in output_paths.items():
+    #    Path(value).mkdir(parents=True, exist_ok=True)
+
+    for item in input_paths:
+        Path(input_paths[item]).mkdir(parents=True, exist_ok=True)
+
+    Path(output_path).mkdir(parents=True, exist_ok=True)
